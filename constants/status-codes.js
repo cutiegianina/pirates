fe3216792d252100c12fd57e65a682e85c074a79
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-03-28 10:15:18 +0800

    initial commit

---
+++ constants/status-codes.js
@@ -0,0 +1,6 @@
+export const StatusCode = {
+    Success: { code: 200, statusPhrase: 'OK' },
+    BadRequest: { code: 400, statusPhrase: 'Bad Reqest' },
+    Forbidden: { code: 400, statusPhrase: 'Forbidden' },
+    NotFound: { code: 404, statusPhrase: 'Not Found'}
+};
---
+++ index.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import { readFile } from 'fs/promises';
+import { StatusCode } from './constants/status-codes.js';
+
+const app = express();
+app.use(express.json());
+
+const users = await readFile('./data/users.json', 'utf-8');
+
+const createHashMap = (data) =>  {
+    const tempData = {};
+    data.forEach(data => {
+        const id = data.id;
+        if (tempData[id]) {
+            if (!Array.isArray(tempData[id])) {
+                tempData[id] = [tempData[id]];
+            }
+            tempData[id].push(data);
+        } else {
+            tempData[id] = data;
+        }
+    });
+    return tempData;
+}
+
+const jsonUserData = JSON.parse(users).Users;
+const userData = createHashMap(jsonUserData);
+
+app.get('/get-users', async (req, res) => { 
+    const response = Object.values(userData);
+    if (response == null) {
+        res.status(StatusCode.NotFound.code).end(StatusCode.NotFound.statusPhrase);
+        return;
+    }
+    res.send(response); 
+});
+
+app.get('/get-user/:id', async (req, res) => {
+    var user = userData[req.params.id];
+    if (user == null) {
+        res.status(StatusCode.NotFound.code).end(StatusCode.NotFound.statusPhrase);
+        return;
+    }
+    res.send(user);
+});
+
+app.post('/create-user', (req, res) => {
+    const newUser = req.body;
+    userData[newUser.id] = newUser;
+    res.send(response);
+});
+
+app.delete('/delete-user/:id', (req, res) => {
+    if (userData[req.params.id] == null) {
+        res.status(StatusCode.NotFound.code).end(StatusCode.NotFound.statusPhrase);
+        return;
+    }
+    delete userData[req.params.id];
+    res.send(StatusCode.Success.statusPhrase);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => { console.log(`Server is running on port ${port}`)});
