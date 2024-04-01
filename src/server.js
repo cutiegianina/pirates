c52e99c86781a225ea40a2124626cc2a80030efc
VPPI\richard.puig <richard.puig@veritaspay.com>
2024-04-01 09:51:41 +0800

    fixing vercel

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { connectToDb } from './config/database.js';
 import { authenticateAPIKey } from './middlewares/authorize.js';
 import { globalErrorHandler } from './middlewares/error-handler.js';
-import pirateController from './controllers/pirateController.js';
+import pirateRouter from './controllers/pirateController.js';
 
 export const app = express();
 
@@ -24,7 +24,7 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use('/api/pirates', pirateController);
+app.use('/api/pirates', pirateRouter);
 
 //app.use(authenticateAPIKey);
 
